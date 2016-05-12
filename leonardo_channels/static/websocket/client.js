e5ac79d25d331dc0640c4f65188c85930ef8a2b7
Adam Tengler <adam.tengler@tcpcloud.eu>
2016-05-12 16:49:34 +0200

    Use newer version of reconnecting-websocket.js

--- leonardo_channels/static/websocket/client.js
+++ leonardo_channels/static/websocket/client.js
@@ -4,7 +4,12 @@
 TODO: make leonardo client which accepts session key and make subscription to a topic
 */
 var ws_scheme = window.location.protocol == 'https:' ? 'wss' : 'ws';
-var basepath = ws_scheme + "://" + window.location.hostname + ":" + window.location.port;
+
+if (window.location.port) {
+    var basepath = ws_scheme + "://" + window.location.hostname + ":" + window.location.port;
+} else {
+    var basepath = ws_scheme + "://" + window.location.hostname
+}
 
 socket = new ReconnectingWebSocket(basepath + "/messages?session_key={{ request.session.session_key }}");
 
@@ -12,7 +17,4 @@ socket = new ReconnectingWebSocket(basepath + "/messages?session_key={{ request.
 socket.onmessage = function(e) {
     msg = JSON.parse(e.data)
     horizon.alert(msg.level, msg.message, msg.extra_tags)
-}
-socket.onopen = function() {
-    //
 }
